fd56fb94a364ff854295b2ff1f988af84bdafc80
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-10-28 00:43:03 +0100

    created an auth user class for the app

---
+++ lib/models/auth_user.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthUser {
+  final String uid;
+  final String displayName;
+  final String email;
+  final bool isEmailVerified;
+
+  AuthUser._private({
+    required this.uid,
+    required this.displayName,
+    required this.email,
+    required this.isEmailVerified,
+  });
+
+  factory AuthUser.fromFirebase(User user) => AuthUser._private(
+      uid: user.uid,
+      displayName: user.displayName!,
+      email: user.email!,
+      isEmailVerified: user.emailVerified);
+}
