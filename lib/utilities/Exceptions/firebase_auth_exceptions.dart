4d6fb38f07dc177883c992396f9b8dddafb7b896
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-10-28 00:48:45 +0100

    added a firebase auth exceptions to be throw for all firebase auth services when there is an error

---
+++ lib/utilities/Exceptions/firebase_auth_exceptions.dart
@@ -0,0 +1,17 @@
+//registeration exceptions
+class EmailAlreadyInUseException implements Exception {}
+
+class InvalidEmailException implements Exception {}
+
+class WeakPasswordException implements Exception {}
+
+//login exceptions
+class UserNotFoundException implements Exception {}
+
+class WrongPasswordException implements Exception {}
+
+class UserDisabledException implements Exception {}
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInException implements Exception {}
