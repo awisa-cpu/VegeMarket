8cbf7c99bc55aeebbb0544c9c1f7a6589096406e
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-10-30 00:41:55 +0100

    added the facebooka and google sifn in option as well as the login button

--- lib/Views/auth_views/sign_in.dart
+++ lib/Views/auth_views/sign_in.dart
@@ -30,67 +30,179 @@ class _SignInViewState extends State<SignInView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(14.5),
-        child: Column(
-          children: [
-            //
-            const SizedBox(
-              height: 200.5,
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(14.5),
+          child: Column(
+            children: [
+              //
+              const SizedBox(
+                height: 200.5,
+              ),
 
-            //message
-            const Text('Login To Your Account'),
+              //message
+              const Text('Login To Your Account'),
 
-            //
-            const SizedBox(
-              height: 25.5,
-            ),
+              //
+              const SizedBox(
+                height: 25.5,
+              ),
 
-            //email field
-            TextField(
-              controller: _emailCon,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Email',
-                contentPadding: EdgeInsets.symmetric(horizontal: 10.5),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10.5),
+              //email field
+              TextField(
+                controller: _emailCon,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10.5),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.5),
+                    ),
                   ),
                 ),
               ),
-            ),
-            //
-            const SizedBox(
-              height: 15.5,
-            ),
+              //
+              const SizedBox(
+                height: 15.5,
+              ),
 
-            //password field
-            TextField(
-              controller: _passwordCon,
-              obscureText: _showPassword,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                contentPadding: const EdgeInsets.symmetric(horizontal: 10.5),
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10.5),
+              //password field
+              TextField(
+                controller: _passwordCon,
+                obscureText: _showPassword,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10.5),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.5),
+                    ),
                   ),
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _showPassword = !_showPassword;
-                    });
-                  },
-                  icon: Icon(
-                    _showPassword ? Icons.visibility_off : Icons.visibility,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _showPassword = !_showPassword;
+                      });
+                    },
+                    icon: Icon(
+                      _showPassword ? Icons.visibility_off : Icons.visibility,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+
+              //
+              const SizedBox(
+                height: 20.5,
+              ),
+              const Text('Or Continue With'),
+
+              const SizedBox(
+                height: 10.5,
+              ),
+
+              //other sign in options
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //
+                  Material(
+                    borderRadius: BorderRadius.circular(10),
+                    elevation: 1.0,
+                    child: Container(
+                      width: 130,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            'lib/images/facebook.png',
+                            width: 20.5,
+                            height: 20.5,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          const Text(
+                            'Facebook ',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  //
+                  const SizedBox(
+                    width: 18,
+                  ),
+
+                  //
+                  Material(
+                    borderRadius: BorderRadius.circular(10),
+                    elevation: 1.0,
+                    child: Container(
+                      width: 130,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            'lib/images/google.jpg',
+                            width: 22.5,
+                            height: 22.5,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          const Text(
+                            'Google ',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+              //
+              const SizedBox(
+                height: 15.5,
+              ),
+              Text(
+                'Forgot Your Password?',
+                style: TextStyle(color: Colors.amber.shade900),
+              ),
+
+              const SizedBox(
+                height: 10.5,
+              ),
+
+              //login button
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 130,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.amber.shade900,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: const Text(
+                    'Login ',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
