addcff94272d7e197368747c24e055040004552b
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-10-30 23:41:19 +0100

    added the rating and more description text

--- lib/services/database/product_repository.dart
+++ lib/services/database/product_repository.dart
@@ -3,112 +3,132 @@ import 'package:shopcart/models/product.dart';
 class ProductsRepo {
   final List<Product> _products = [
     Product(
-      id: 1,
-      categoryId: 1,
-      name: 'Potato',
-      price: 1.5,
-      description: 'Thi is is a carbohydrate food',
-      image: 'lib/images/products/potato.png',
-      weight: 1.32
-    ),
+        id: 1,
+        categoryId: 1,
+        name: 'Potato',
+        price: 1.5,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        image: 'lib/images/products/potato.png',
+        weight: 1.32,
+        rating: 4.0),
     Product(
-      id: 2,
-      categoryId: 2,
-      name: 'Banana',
-      price: 1.32,
-      description: 'Thi is is a carbohydrate food',
-      image: 'lib/images/products/banana.png',
-      weight: 1.32
-    ),
+        id: 2,
+        categoryId: 2,
+        name: 'Banana',
+        price: 1.32,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        image: 'lib/images/products/banana2.png',
+        weight: 1.32,
+        rating: 4.7),
     Product(
       id: 3,
       categoryId: 3,
       name: 'Lettuce',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/lettuce.png',
-      weight: 1.32
+      weight: 1.32,
+      rating: 3.9,
     ),
     Product(
       id: 4,
       categoryId: 3,
       name: 'Cabbage',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/cabbage.png',
-       weight: 0.82,
+      weight: 0.82,
+      rating: 3.8,
     ),
     Product(
-      id: 5,
-      categoryId: 2,
-      name: 'Tomatoes',
-      price: 1.5,
-      description: 'Thi is is a carbohydrate food',
-      image: 'lib/images/products/tomatoes.png',
-      weight: 1.35
-    ),
+        id: 5,
+        categoryId: 2,
+        name: 'Tomatoes',
+        price: 1.5,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        image: 'lib/images/products/tomatoes.png',
+        weight: 1.35,
+        rating: 4.5),
     Product(
-      id: 6,
-      categoryId: 2,
-      name: 'Egg Plant',
-      price: 1.5,
-      description: 'Thi is is a carbohydrate food',
-      image: 'lib/images/products/eggplant.png',
-       weight: 0.82
-    ),
+        id: 6,
+        categoryId: 2,
+        name: 'Egg Plant',
+        price: 1.5,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        image: 'lib/images/products/eggplant.png',
+        weight: 0.82,
+        rating: 4.0),
     Product(
       id: 1,
       categoryId: 1,
       name: 'Potato',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/potato.png',
-      weight: 1.32
+      weight: 1.32,
+      rating: 4.0,
     ),
     Product(
       id: 2,
       categoryId: 2,
       name: 'Banana',
       price: 1.32,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/banana.png',
-      weight: 1.32
+      weight: 1.32,
+      rating: 4.7,
     ),
     Product(
       id: 3,
       categoryId: 3,
       name: 'Lettuce',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/lettuce.png',
-      weight: 1.32
+      weight: 1.32,
+      rating: 3.9,
     ),
     Product(
       id: 4,
       categoryId: 3,
       name: 'Cabbage',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/cabbage.png',
-       weight: 0.82
+      weight: 0.82,
+      rating: 3.8,
     ),
     Product(
       id: 5,
       categoryId: 2,
       name: 'Tomatoes',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/tomatoes.png',
-      weight: 1.35
+      weight: 1.35,
+      rating: 4.5,
     ),
     Product(
       id: 6,
       categoryId: 2,
       name: 'Egg Plant',
       price: 1.5,
-      description: 'Thi is is a carbohydrate food',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
       image: 'lib/images/products/eggplant.png',
-       weight: 0.82
+      weight: 0.82,
+      rating: 4.0,
     ),
   ];
 
