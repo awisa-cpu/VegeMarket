d08eccd036c84cc7904c95af2bfa128eec2046cd
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-11-01 15:33:19 +0100

    nested the state management to handle the states properly

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:shopcart/Controllers/auth/auth_bloc_controller.dart';
-import 'package:shopcart/Controllers/auth/auth_event.dart';
-import 'package:shopcart/Controllers/auth/auth_state.dart';
-import 'package:shopcart/Controllers/product_controller/product_controller.dart';
-import 'package:shopcart/Views/auth_views/sign_in.dart';
-import 'package:shopcart/Views/auth_views/sign_up.dart';
-import 'package:shopcart/Views/product_views/home_product.dart';
+import 'package:shopcart/Controllers/product_controller/product_provider.dart';
 import 'package:shopcart/services/auth/firebase_auth_provider.dart';
 import 'package:shopcart/utilities/routes/route_generator.dart';
-import 'package:shopcart/utilities/displays/snackbar.dart';
+
+import 'Views/view_determinator.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,64 +19,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AuthBloc(provider: FirebaseAuthProvider()),
-      child: MaterialApp(
-        onGenerateRoute: (settings) => generateRoute(settings),
-        title: 'Vege Market',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber.shade900),
-          useMaterial3: true,
-        ),
-        home: const ViewDeterminator(),
-      ),
-    );
-  }
-}
+    return BlocProvider<AuthenticationBloc>(
 
-class ViewDeterminator extends StatelessWidget {
-  const ViewDeterminator({super.key});
+      //to provide access to auth provider
+      create: (_) => AuthenticationBloc(provider: FirebaseAuthProvider()),
 
-  @override
-  Widget build(BuildContext context) {
-    context.read<AuthBloc>().add(AuthEventInitialize());
+      //to provice access to the product provider
+      child: ChangeNotifierProvider(
+        create: (_) => ProductProvider(),
 
-    //
-    return BlocConsumer<AuthBloc, AuthState>(
-      builder: (context, state) {
         //
-        if (state is AuthStateLoggedIn) {
-          return ChangeNotifierProvider(
-            create: (context) => ProductProvider(),
-            child: const HomeProduct(),
-            
-          );
-
-          //
-        } else if (state is AuthStateLoggedOut) {
-          return const SignInView();
-
-          //
-        } else if (state is AuthStateHaveAccount) {
-          return const SignInView();
-        } else if (state is AuthStateDontHaveAccount) {
-          return const SignUpView();
-        } else {
-          return const Scaffold(
-            body: Center(
-              child: Text('State problem'),
-            ),
-          );
-        }
-      },
-      listener: (context, state) {
-        if (state is AuthStateLoggedOut) {
-          if (state.isLoading == true) {
-            displaySnackBar(context: context, text: 'loading ....');
-          }
-        }
-      },
+        child: MaterialApp(
+          onGenerateRoute: (settings) => generateRoute(settings),
+          title: 'Vege Market',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber.shade900),
+            useMaterial3: true,
+          ),
+          home: const ViewDeterminator(),
+        ),
+      ),
     );
   }
 }
