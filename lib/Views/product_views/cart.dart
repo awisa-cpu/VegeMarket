11464215476b118178f650ff99dd3f8ffe854b96
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-11-05 00:04:03 +0100

    added the list view , clear cart function and the prices

--- lib/Views/product_views/cart.dart
+++ lib/Views/product_views/cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopcart/Controllers/product_controller/product_provider.dart';
+import 'package:shopcart/models/product.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -18,13 +19,127 @@ class _CartPageState extends State<CartPage> {
           'Cart',
           style: TextStyle(fontSize: 20),
         ),
+        actions: [
+          Stack(
+            children: [
+              //
+              Positioned(
+                left: 15,
+                child: Container(
+                  height: 25,
+                  width: 25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.green,
+                  ),
+                  child: Center(
+                    child: Consumer<ProductProvider>(
+                      builder: (context, value, _) {
+                        return Text(
+                          value.totalSelectedItems.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+              //
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  //route to the carts page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CartPage(),
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  Icons.shopping_cart,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        child: Consumer<ProductProvider>(
-          builder: (context, value, child) =>
-              Text(value.totalSelectedItems.toString()),
-        ),
+
+      //
+      body: Consumer<ProductProvider>(
+        builder: (context, value, _) {
+          //
+          if (value.cart.isEmpty) {
+            return const Center(
+              child: Text('Your cart is Empty'),
+            );
+          } else {
+            //
+            return Column(
+              children: [
+                //
+                Container(
+                  color: Colors.grey.shade300,
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Subtotal'),
+                      Text("\$ ${value.totalPrice.toString()}")
+                    ],
+                  ),
+                ),
+
+                //
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: value.cart.length,
+                  itemBuilder: (context, index) {
+                    //
+                    final product = value.cart[index];
+
+                    //
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //
+                        ListTile(
+                          leading: Image.asset(product.image ?? 'no image'),
+                          title: Text(product.name),
+                          subtitle: Text(product.price.toString()),
+                        ),
+
+                        //
+                        TextButton(
+                          onPressed: () => _removeFromCart(product: product),
+                          child: const Text('Remove'),
+                        )
+                      ],
+                    );
+                  },
+                ),
+
+                //
+                TextButton(
+                    onPressed: _clearCart, child: const Text('Clear Cart')),
+              ],
+            );
+          }
+        },
       ),
     );
+  }
+
+  void _removeFromCart({required Product product}) {
+    context.read<ProductProvider>().removeFromCart(product: product);
+  }
+
+  void _clearCart() {
+    context.read<ProductProvider>().clearCart();
   }
 }
