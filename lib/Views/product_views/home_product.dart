62173f0c5e79e4d5aaa874440319bd2da6827148
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-11-06 01:36:53 +0100

    added the message image to the homepage

--- lib/Views/product_views/home_product.dart
+++ lib/Views/product_views/home_product.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopcart/Controllers/product_controller/product_provider.dart';
-import 'package:shopcart/Views/product_views/cart.dart';
+import 'package:shopcart/Views/app_views/cart.dart';
 import 'package:shopcart/Views/product_views/single_product.dart';
 
 class HomeProduct extends StatefulWidget {
@@ -34,11 +34,11 @@ class _HomeProductState extends State<HomeProduct> {
               const SizedBox(
                 height: 10,
               ),
+
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   //
-                  IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
 
                   Stack(
                     children: [
@@ -73,7 +73,7 @@ class _HomeProductState extends State<HomeProduct> {
                         enableFeedback: false,
                         onPressed: () {
                           //route to the carts page
-                          
+
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -88,8 +88,6 @@ class _HomeProductState extends State<HomeProduct> {
                       ),
                     ],
                   ),
-                
-                
                 ],
               ),
 
@@ -115,8 +113,59 @@ class _HomeProductState extends State<HomeProduct> {
                     fontWeight: FontWeight.bold),
               ),
 
+              //
+              Padding(
+                padding: const EdgeInsets.all(8.5),
+                child: Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.5),
+                    color: Colors.yellow.shade700,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, top: 5, bottom: 5),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Stay home',
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.white,
+                                  wordSpacing: -1),
+                            ),
+                            Text(
+                              'We deliver',
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white),
+                            ),
+                            Text(
+                              'Any where... Any time!',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+
+                        Image.asset(
+                          'lib/images/products/fruits.png',
+                          height: 500,
+                          width: 190,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
               //all products
               GridView.builder(
+                padding: EdgeInsets.zero,
                 physics: const ScrollPhysics(),
                 shrinkWrap: true,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
