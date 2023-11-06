99d44b42ce6153a24a527479c11ae42e1e5de291
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-11-06 01:37:51 +0100

    added the favourite icon

--- lib/Views/product_views/single_product.dart
+++ lib/Views/product_views/single_product.dart
@@ -1,11 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopcart/Controllers/product_controller/product_provider.dart';
 import 'package:shopcart/Views/product_views/product_detailed_page.dart';
 import 'package:shopcart/models/product.dart';
 
-class SingleProduct extends StatelessWidget {
+class SingleProduct extends StatefulWidget {
   const SingleProduct({
     super.key,
     required this.product,
@@ -14,88 +13,115 @@ class SingleProduct extends StatelessWidget {
   final Product product;
 
   @override
+  State<SingleProduct> createState() => _SingleProductState();
+}
+
+class _SingleProductState extends State<SingleProduct> {
+  bool fav = false;
+  @override
   Widget build(BuildContext context) {
     final provider = context.read<ProductProvider>();
     //
     return Material(
       elevation: 1.0,
-      borderRadius: BorderRadius.circular(10),
+      borderRadius: BorderRadius.circular(15),
+      color: Colors.grey.shade100,
       child: SizedBox(
-        height: 200,
-        width: 200,
+        height: 210,
+        width: 210,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            GestureDetector(
-                onTap: () {
-                  //
-
-                  // Navigator.of(context).pushNamedAndRemoveUntil(
-                  //     detailedProduct, (route) => false,
-                  //     arguments: product);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            ProductDetailedPage(product: product),
-                      ));
-                },
-                child: Image.asset(
-                  product.image!,
-                  height: 70,
-                  width: 90,
-                )),
-
-            //
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  product.name,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 17),
-                ),
-                Text('by weight ${product.weight ?? 0.0} kg'),
-              ],
-            ),
-
-            //
-            const SizedBox(
-              height: 18.4,
-            ),
-
-            //
+            //favourite
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text('\$${product.price.toString()}'),
-
-                //
-                GestureDetector(
-                  onTap: () {
-                    //add to cart
-                    provider.addToCart(product: product);
-                  },
-                  child: Container(
-                    height: 23,
-                    width: 22,
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(5.5),
-                    ),
-                    child: const Icon(
-                      Icons.add,
-                      size: 15,
-                      color: Colors.white,
-                    ),
+                IconButton(
+                  enableFeedback: false,
+                  padding: EdgeInsets.zero,
+                  onPressed: () => _addToFavourite(product: widget.product),
+                  icon: Icon(
+                    Icons.favorite,
+                    color: fav ? Colors.red : Colors.green,
                   ),
                 )
               ],
             ),
+
+            //image
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ProductDetailedPage(product: widget.product),
+                    ));
+              },
+              child: Image.asset(
+                widget.product.image!,
+                height: 70,
+                width: 90,
+              ),
+            ),
+
+            //other options
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //name
+                  Text(
+                    widget.product.name,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+
+                  //price
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('\$${widget.product.price}/ kg'),
+
+                      //add to cart
+                      GestureDetector(
+                        onTap: () {
+                          provider.addToCart(product: widget.product);
+                        },
+                        child: Container(
+                          height: 23,
+                          width: 22,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(5.5),
+                          ),
+                          child: const Icon(
+                            Icons.add,
+                            size: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            //
           ],
         ),
       ),
     );
+  }
+
+  void _addToFavourite({required Product product}) {
+    setState(() {
+      fav = true;
+    });
+    context.read<ProductProvider>().addToFavourite(product: product);
   }
 }
