aa0ffba2b6414034ed366f65edb54f1496ea346c
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-11-06 22:55:55 +0100

    added a callback and a tect field to the widget

--- lib/utilities/widgets/buy_button.dart
+++ lib/utilities/widgets/buy_button.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 
+typedef OnClick = void Function();
+
 class BuyButton extends StatelessWidget {
+  final String? text;
+  final OnClick? onPressed;
+
+  //
   const BuyButton({
     super.key,
+    this.text,
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: 40,
-      width: 110,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(111, 207, 151, 1),
-        borderRadius: BorderRadius.circular(20.5),
-      ),
-      child: const Text(
-        'Buy now',
-        style: TextStyle(
-            color: Colors.white, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        alignment: Alignment.center,
+        height: 40,
+        width: 120,
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(111, 207, 151, 1),
+          borderRadius: BorderRadius.circular(20.5),
+        ),
+        child: Text(
+          text ?? 'Buy now',
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
