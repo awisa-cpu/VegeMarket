4877a01be68ff10435647f439d3993dd790db517
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-12-02 21:31:03 +0100

    added the behaviour and duration property

--- lib/utilities/displays/snackbar.dart
+++ lib/utilities/displays/snackbar.dart
@@ -4,6 +4,11 @@ void displaySnackBar({
   required BuildContext context,
   required String text,
 }) {
-  final snackbar = SnackBar(content: Text(text),backgroundColor: Colors.amber.shade800,);
+  final snackbar = SnackBar(
+    content: Text(text),
+    backgroundColor: Colors.amber.shade800,
+    behavior: SnackBarBehavior.floating,
+    duration: const Duration(seconds: 2),
+  );
   ScaffoldMessenger.of(context).showSnackBar(snackbar);
 }
